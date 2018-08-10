06e1a69d33ffffb2c267e764f53d35ef19323c26
James Bergeron <jimber27@yahoo.com>
2018-08-10 02:25:26 -0400

    test

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,7 +9,7 @@ const path = require('path');
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'../../client/build/index.html'));
+  res.sendFile(path.join(__dirname+'../../../client/build/index.html'));
 });
 
 
